8eaa1a0974c2be5c3dac7708694c90d55c226198
Kevin Harwood <kevin.harwood@mutualmobile.com>
2013-10-28 13:26:42 -0500

    Removed warnings, and streamlined code base.

--- KHGravatar/KHGravatar.m
+++ KHGravatar/KHGravatar.m
@@ -33,18 +33,18 @@ static NSString * KHGravatarHashForEmailAddress(NSString *emailAddress) {
     }
     emailAddress = [emailAddress stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     emailAddress = [emailAddress lowercaseString];
-    
+
     const char *cStr = [emailAddress UTF8String];
-    unsigned char result[16];
-    CC_MD5( cStr, strlen(cStr), result );
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5( cStr, (CC_LONG)strlen(cStr), result );
     NSString* md5EmailAddress =  [NSString stringWithFormat:
-                                  @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+                                  @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
                                   result[0], result[1], result[2], result[3],
                                   result[4], result[5], result[6], result[7],
                                   result[8], result[9], result[10], result[11],
                                   result[12], result[13], result[14], result[15]
                                   ];
-    return [md5EmailAddress lowercaseString];
+    return md5EmailAddress;
 }
 
 static NSString * KHRatingStringForRating(KHGravatarRating rating){
